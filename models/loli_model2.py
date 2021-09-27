b1a53f628dfd686228e2220a04bc68113b89b781
AllanKamimura <65671989+AllanKamimura@users.noreply.github.com>
2021-09-27 13:23:47 -0300

    formatação tava zuada

--- models/loli_model2.py
+++ models/loli_model2.py
@@ -6,11 +6,11 @@ def base_model(shape):
 	input_layer = tf.keras.layers.Input(shape = shape, name = "input_layer")
 	
 	mobile_model = tf.keras.applications.MobileNetV3Small(include_top = False,
-														  input_shape = shape,
-														  pooling = "max",
-														  weights = "imagenet",
-														  dropout_rate = 0.1,
-														  )
+							      input_shape = shape,
+							      pooling = "max",
+							      weights = "imagenet",
+							      dropout_rate = 0.1,
+							     )
 	y = mobile_model(input_layer)
 
 	model = tf.keras.models.Model(
@@ -47,15 +47,13 @@ def loli_model():
 
 def predict(image_path, model = "dummy"):
 	if model == "dummy":
-    	model = loli_model()
-    
-    in_folder = in_folder
-    
-    image = tf.keras.preprocessing.image.load_img(image_path, 
-												  target_size = (height, width))
-    
-    image = tf.keras.preprocessing.image.img_to_array(image)
-    image_arr = tf.expand_dims(image, axis = 0)
-    
-    predicts = model(image_arr)
+		model = loli_model()
+	in_folder = in_folder
+	image = tf.keras.preprocessing.image.load_img(image_path, 
+						      target_size = (height, width))
+
+	image = tf.keras.preprocessing.image.img_to_array(image)
+	image_arr = tf.expand_dims(image, axis = 0)
+
+	predicts = model(image_arr)
     return tf.argmax(predicts[0]).numpy()
